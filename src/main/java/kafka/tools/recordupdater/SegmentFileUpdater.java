cd28df6302002d4a98b58a4619e25dfe501d804e
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2017-03-20 19:31:05 -0700

    Implemented CRC calculation

--- src/main/java/kafka/tools/recordupdater/SegmentFileUpdater.java
+++ src/main/java/kafka/tools/recordupdater/SegmentFileUpdater.java
@@ -1,14 +1,17 @@
 package kafka.tools.recordupdater;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.zip.CRC32;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.math.IntMath;
+import com.google.common.math.LongMath;
 import com.google.common.primitives.UnsignedBytes;
 
 import kafka.tools.recordupdater.api.RecordUpdater;
@@ -51,67 +54,35 @@ public class SegmentFileUpdater {
     private final byte[] messageKeyLength = new byte[LENGTH_BYTES];
     private final byte[] messageValueLength = new byte[LENGTH_BYTES];
     private byte messageMagicValue;
+    private final boolean verifyCrc;
+    private long recordsVisited = 0;
+    private long recordsUpdated = 0;
 
     public SegmentFileUpdater(File file) {
+        this(file, false);
+    }
+
+    public SegmentFileUpdater(File file, boolean verifyCrc) {
         this.file = file;
+        this.verifyCrc = verifyCrc;
     }
 
     public boolean run(RecordUpdater recordUpdater) throws FileNotFoundException, IOException {
-        long recordsVisited = 0;
-        long recordsUpdated = 0;
-
         try (final RandomAccessFile raf = new RandomAccessFile(file, "rwd")) {
             while (true) {
-                // populate bytes
-                if (raf.read(messageOffset) == -1) {
-                    // eof
+                try {
+                    if (handleNextRecord(recordUpdater, raf)) {
+                        recordsVisited++;
+                    } else {
+                        break;
+                    }
+                } catch (EOFException e) {
+                    logger.warn("Unexpected EOF at record no. {} in {}", recordsVisited + 1, file);
                     break;
-                }
-                recordsVisited++;
-
-                final long offset = getLong(messageOffset);
-
-                raf.read(messageLength);
-                raf.read(messageCrc);
-                messageMagicValue = (byte) raf.read();
-
-                // read past the 'attributes' byte which we don't care about
-                raf.read();
-
-                if (messageMagicValue > 0) {
-                    raf.read(messageTimestamp);
-                }
-                raf.read(messageKeyLength);
-
-                final int keyLength = getInteger(messageKeyLength);
-                final byte[] messageKey;
-                if (keyLength == -1) {
-                    messageKey = new byte[0];
-                } else {
-                    messageKey = new byte[keyLength];
-                    raf.readFully(messageKey);
-                }
-                
-                raf.readFully(messageValueLength);
-                final int valueLength = getInteger(messageValueLength);
-                final byte[] messageValue = new byte[valueLength];
-                raf.readFully(messageValue);
-
-                if (recordUpdater.update(offset, messageKey, messageValue)) {
-                    final long filePointer = raf.getFilePointer();
-                    final long messageValueOffset = filePointer - valueLength;
-                    final long messageKeyOffset = messageValueOffset - LENGTH_BYTES - keyLength;
-                    
-                    // TODO: Update CRC - "The CRC is the CRC32 of the remainder of the message bytes. This is used to check the integrity of the message on the broker and consumer."
-
-                    raf.seek(messageKeyOffset);
-                    raf.write(messageKey);
-                    raf.seek(messageValueOffset);
-                    raf.write(messageValue);
-
-                    logger.debug("Updated record with offset={} in segment file: {}", offset, file);
-
-                    recordsUpdated++;
+                } catch (IOException e) {
+                    logger.error("Failed to process record no. {} in {}: {} - {}", recordsVisited + 1, file, e
+                            .getClass().getName(), e.getMessage());
+                    throw e;
                 }
             }
         }
@@ -119,12 +90,131 @@ public class SegmentFileUpdater {
         return recordsUpdated > 0;
     }
 
+    public long getRecordsUpdated() {
+        return recordsUpdated;
+    }
+
+    public long getRecordsVisited() {
+        return recordsVisited;
+    }
+
+    private boolean handleNextRecord(RecordUpdater recordUpdater, RandomAccessFile raf) throws IOException {
+        // populate bytes
+        if (raf.read(messageOffset) == -1) {
+            // eof
+            return false;
+        }
+
+        final long offset = getLong(messageOffset);
+
+        raf.read(messageLength);
+
+        final long crcOffset = raf.getFilePointer();
+        raf.read(messageCrc);
+        messageMagicValue = (byte) raf.read();
+
+        // read past the 'attributes' byte which we don't care about
+        byte attributes = (byte) raf.read();
+
+        if (messageMagicValue > 0) {
+            raf.read(messageTimestamp);
+        }
+        raf.read(messageKeyLength);
+        
+        final long messageKeyOffset = raf.getFilePointer();
+
+        final int keyLength = getInteger(messageKeyLength);
+        final byte[] messageKey;
+        if (keyLength == -1) {
+            messageKey = new byte[0];
+        } else {
+            messageKey = new byte[keyLength];
+            raf.readFully(messageKey);
+        }
+
+        raf.readFully(messageValueLength);
+        final long messageValueOffset = raf.getFilePointer();
+        
+        final int valueLength = getInteger(messageValueLength);
+        final byte[] messageValue = new byte[valueLength];
+        raf.readFully(messageValue);
+
+        final long existingCrc = getLong(messageCrc);
+
+        if (verifyCrc) {
+            // could be turned on for verification
+            final long calculatedCrc = calculateCrc(attributes, messageKey, messageValue);
+            if (existingCrc != calculatedCrc) {
+                throw new IllegalStateException("Invalid CRC value detected. Expected " + calculatedCrc
+                        + " (calculated) but found " + existingCrc + " (in file)");
+            }
+        }
+
+        if (recordUpdater != null && recordUpdater.update(offset, messageKey, messageValue)) {
+            // Update CRC
+            final long newCrc = calculateCrc(attributes, messageKey, messageValue);
+            if (existingCrc != newCrc) {
+                raf.seek(crcOffset);
+                raf.write(getBytes(newCrc, messageCrc.length));
+            }
+
+            raf.seek(messageKeyOffset);
+            raf.write(messageKey);
+            raf.seek(messageValueOffset);
+            raf.write(messageValue);
+
+            logger.debug("Updated record with offset={} in segment file: {}", offset, file);
+
+            recordsUpdated++;
+        }
+        return true;
+    }
+
+    /**
+     * Calculates the CRC of the current message.
+     * 
+     * From the Kafka documentation: "The CRC is the CRC32 of the remainder of
+     * the message bytes. This is used to check the integrity of the message on
+     * the broker and consumer."
+     * 
+     * @param attributes
+     * @param messageKey
+     * @param messageValue
+     * @return
+     */
+    private long calculateCrc(byte attributes, final byte[] messageKey, final byte[] messageValue) {
+        final CRC32 crc = new CRC32();
+        crc.update(messageMagicValue);
+        crc.update(attributes);
+        if (messageMagicValue > 0) {
+            crc.update(messageTimestamp);
+        }
+        crc.update(messageKeyLength);
+        crc.update(messageKey);
+        crc.update(messageValueLength);
+        crc.update(messageValue);
+        return crc.getValue();
+    }
+
+    protected static byte[] getBytes(long sum, int arraySize) {
+        final byte[] b = new byte[arraySize];
+        for (int i = 0; i < b.length; i++) {
+            final int reverseIndex = b.length - 1 - i;
+            final long factor = LongMath.pow(256, reverseIndex);
+            final long base = sum / factor;
+            b[i] = UnsignedBytes.checkedCast(base);
+            sum = sum - (base * factor);
+        }
+        return b;
+    }
+
     protected static long getLong(byte[] b) {
         long sum = 0;
         for (int i = 0; i < b.length; i++) {
-            int reverseIndex = b.length - 1 - i;
-            int factor = IntMath.pow(256, reverseIndex);
-            sum += UnsignedBytes.toInt(b[i]) * factor;
+            final int reverseIndex = b.length - 1 - i;
+            final long factor = LongMath.pow(256, reverseIndex);
+            final int base = UnsignedBytes.toInt(b[i]);
+            sum += base * factor;
         }
         return sum;
     }
@@ -132,9 +222,10 @@ public class SegmentFileUpdater {
     protected static int getInteger(byte[] b) {
         int sum = 0;
         for (int i = 0; i < b.length; i++) {
-            int reverseIndex = b.length - 1 - i;
-            int factor = IntMath.pow(256, reverseIndex);
-            sum += UnsignedBytes.toInt(b[i]) * factor;
+            final int reverseIndex = b.length - 1 - i;
+            final int factor = IntMath.pow(256, reverseIndex);
+            final int base = UnsignedBytes.toInt(b[i]);
+            sum += base * factor;
         }
         return sum;
     }
--- src/test/java/kafka/tools/recordupdater/SegmentFileUpdaterTest.java
+++ src/test/java/kafka/tools/recordupdater/SegmentFileUpdaterTest.java
@@ -3,18 +3,21 @@ package kafka.tools.recordupdater;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.Arrays;
 
 import org.junit.Test;
 
-import kafka.tools.recordupdater.SegmentFileUpdater;
+import com.google.common.io.Files;
+
 import kafka.tools.recordupdater.api.RecordUpdater;
+import kafka.tools.recordupdater.updaters.DestroyValueRecordUpdater;
 
 public class SegmentFileUpdaterTest {
 
     @Test
-    public void testTraverseFile() throws Exception {
-        File file = new File("src/test/resources/example-log-health-check.log");
-        SegmentFileUpdater updater = new SegmentFileUpdater(file);
+    public void testTraverseFileDespiteCrcIssues() throws Exception {
+        final File file = new File("src/test/resources/example-log-health-check.log");
+        final SegmentFileUpdater updater = new SegmentFileUpdater(file, false);
         updater.run(new RecordUpdater() {
             @Override
             public boolean update(long offset, byte[] key, byte[] value) {
@@ -25,7 +28,7 @@ public class SegmentFileUpdaterTest {
     }
 
     @Test
-    public void testGetLength() {
+    public void testGetLong() {
         assertEquals(16, SegmentFileUpdater.getLong(new byte[] { 16 }));
         assertEquals(16, SegmentFileUpdater.getLong(new byte[] { 0, 16 }));
         assertEquals(16, SegmentFileUpdater.getLong(new byte[] { 0, 0, 0, 0, 16 }));
@@ -35,5 +38,40 @@ public class SegmentFileUpdaterTest {
         assertEquals(256, SegmentFileUpdater.getLong(new byte[] { 1, 0 }));
         assertEquals(272, SegmentFileUpdater.getLong(new byte[] { 1, 16 }));
         assertEquals(528, SegmentFileUpdater.getLong(new byte[] { 2, 16 }));
+    }
+
+    @Test
+    public void testGetBytes() {
+        assertEquals("[16]", Arrays.toString(SegmentFileUpdater.getBytes(16, 1)));
+        assertEquals("[0, 16]", Arrays.toString(SegmentFileUpdater.getBytes(16, 2)));
+        assertEquals("[0, 0, 0, 0, 16]", Arrays.toString(SegmentFileUpdater.getBytes(16, 5)));
+        assertEquals("[0, -107]", Arrays.toString(SegmentFileUpdater.getBytes(149, 2)));
+        assertEquals("[-1]", Arrays.toString(SegmentFileUpdater.getBytes(255, 1)));
+        assertEquals("[1, 0]", Arrays.toString(SegmentFileUpdater.getBytes(256, 2)));
+        assertEquals("[1, 16]", Arrays.toString(SegmentFileUpdater.getBytes(272, 2)));
+        assertEquals("[2, 16]", Arrays.toString(SegmentFileUpdater.getBytes(528, 2)));
+    }
+
+    @Test
+    public void testCalculateCrc() throws Exception {
+        final File workingFile = new File("target/testCalculateCrc.log");
+        Files.copy(new File("src/test/resources/example-log-dir-hello/hello-0/00000000000000000000.log"), workingFile);
+
+        final SegmentFileUpdater verifier1 = new SegmentFileUpdater(workingFile, true);
+        // first verify integrity of original file
+        verifier1.run(null);
+
+        final SegmentFileUpdater updater = new SegmentFileUpdater(workingFile, true);
+        updater.run(new DestroyValueRecordUpdater('!'));
+
+        // re-run verification
+        final SegmentFileUpdater verifier2 = new SegmentFileUpdater(workingFile, true);
+        verifier2.run(new RecordUpdater() {
+            @Override
+            public boolean update(long offset, byte[] key, byte[] value) {
+                assertEquals('!', value[0]);
+                return false;
+            }
+        });
     }
 }
